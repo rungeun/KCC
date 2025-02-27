2880d26fe8e45ef54dcfbb916e78f3718ae6dcb7
김정근 <132816679+rungeun@users.noreply.github.com>
2025-02-27 22:19:33 +0900

    feat: git init

---
+++ src/main/java/com/example/KCC/KccApplication.java
@@ -0,0 +1,13 @@
+package com.example.KCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KCC/KccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
