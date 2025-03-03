3c1091335734b37491aa5232c781b15beff2c01b
Jika8183 <kimjiseob1209@gmail.com>
2025-03-03 23:11:00 +0900

    feat: 모든 포스트(게시글) 조회 API
    - GET 이용

---
+++ src/main/java/com/example/KCC/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.example.KCC.repository;
+
+import com.example.KCC.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
